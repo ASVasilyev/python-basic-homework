f9a83c493157445c0a85bd72aba39b15397bb611
Alexander Vasilyev <admiral40rus@gmail.com>
2024-11-14 01:23:27 +0300

    Вторая функция (с фильтром)

--- homework_01/old_version/main.py
+++ homework_01/old_version/main.py
@@ -1,3 +1,4 @@
+from mpmath.libmp import isprime
 """
 Домашнее задание №1
 Функции и структуры данных
@@ -22,16 +23,24 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def my_is_prime(number):
+    if isprime(number) == True:
+        return number
 
-def filter_numbers():
+def filter_numbers(list_numbers, type_filter):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
 
     >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
+    [1, 3]
     >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
+    [2, 4]
     """
-    # здесь будет тело функции 2
+    if type_filter == EVEN:
+        return list(filter(lambda i: i % 2 == 0, list_numbers))
+    elif type_filter == ODD:
+        return list(filter(lambda i: i % 2 != 0, list_numbers))
+    elif type_filter == PRIME:
+        return list(filter(my_is_prime, list_numbers))
